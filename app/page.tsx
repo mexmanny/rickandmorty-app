ae20340b6d6e40cd25a6ab6c1afa23f41a4e2220
Manny <manuelrivera09@gmail.com>
2023-10-30 18:39:24 -0400

    updating type definition for initial state of urls

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import EpisodeList from './components/EpisodeList';
 import { useState } from 'react';
 
 export default function Home() {
-  const [characterUrls, setCharacterUrls] = useState([]);
+  const [characterUrls, setCharacterUrls] = useState<string[]>([]);
   return (
     <main className="flex w-screen h-screen overflow-hidden">
       <div className="w-[300px] p-8 h-screen overflow-y-auto">
         <EpisodeList setCharacterUrls={setCharacterUrls} />
       </div>
-      <div className="w-full grid md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5 gap-8 p-8 h-screen overflow-y-auto">
+      <div className="w-full grid md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5 gap-8 p-8 h-screen">
         <CharacterList characterUrls={characterUrls} />
       </div>
     </main>
